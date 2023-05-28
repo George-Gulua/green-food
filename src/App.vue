66af9e847aed26edfe6d9b00b44026c22fd43d80
george <georgegulua@mail.ru>
2023-05-28 20:05:56 +0300

    second commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <the-header/>
+    <router-view/>
+    <the-footer/>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader/index.vue'
+import TheFooter from './components/TheFooter/index.vue'
+
+export default {
+  name: 'App',
+  components: {TheHeader, TheFooter}
+}
+</script>
+
+<style lang="scss">
+@import 'assets/scss/globals.scss';
+</style>
